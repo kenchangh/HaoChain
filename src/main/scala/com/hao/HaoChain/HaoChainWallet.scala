e6080a23b88a6ec7773ac902e934d577c949c93d
mavenave <guanhao3797@gmail.com>
2018-06-10 03:20:48 +0800

    added passwordfield

--- src/main/scala/com/hao/HaoChain/HaoChainWallet.scala
+++ src/main/scala/com/hao/HaoChain/HaoChainWallet.scala
@@ -4,6 +4,7 @@ import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
+import scalafx.scene.control.{PasswordField, TextField}
 import scalafx.scene.effect.DropShadow
 import scalafx.scene.layout.HBox
 import scalafx.scene.paint.{LinearGradient, Stops}
@@ -14,34 +15,41 @@ import scalafx.scene.paint.Color._
 object HaoChainWallet extends JFXApp {
 
   stage = new PrimaryStage {
-    title = "ScalaFX Hello World"
+    title = "HaoChain Wallet"
     scene = new Scene(850, 500) {
       fill = Black
-      content = new HBox {
-        padding = Insets(20)
-        children = Seq(
-          new Text {
-            text = "Hello "
-            style = "-fx-font-size: 48pt"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(PaleGreen, SeaGreen))
-          },
-          new Text {
-            text = "World!!!"
-            style = "-fx-font-size: 48pt"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Cyan, DodgerBlue)
-            )
-            effect = new DropShadow {
-              color = DodgerBlue
-              radius = 25
-              spread = 0.25
+      content = Seq(
+        new HBox {
+          padding = Insets(20)
+          children = Seq(
+            new Text {
+              text = "Hello "
+              style = "-fx-font-size: 48pt"
+              fill = new LinearGradient(
+                endX = 0,
+                stops = Stops(PaleGreen, SeaGreen))
+            },
+            new Text {
+              text = "World!!!"
+              style = "-fx-font-size: 48pt"
+              fill = new LinearGradient(
+                endX = 0,
+                stops = Stops(Cyan, DodgerBlue)
+              )
+              effect = new DropShadow {
+                color = DodgerBlue
+                radius = 25
+                spread = 0.25
+              }
             }
-          }
-        )
-      }
+          )
+        }, new HBox {
+          padding = Insets(20)
+          children = Seq(
+            new PasswordField {
+            }
+          )
+        })
     }
   }
 }
