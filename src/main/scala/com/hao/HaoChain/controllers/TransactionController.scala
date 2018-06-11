f507671c2488695f187f02c87c3c1212ea8c95d7
mavenave <guanhao3797@gmail.com>
2018-06-12 02:34:28 +0800

    TransactionController.generateTransaction done

--- src/main/scala/com/hao/HaoChain/controllers/TransactionController.scala
+++ src/main/scala/com/hao/HaoChain/controllers/TransactionController.scala
@@ -1,11 +1,17 @@
 package com.hao.HaoChain.controllers
 
-import com.hao.HaoChain.core.{Account, Transaction}
+import java.security.PublicKey
+
+import com.hao.HaoChain.core.{Account, GlobalAccountState, StringUtils, Transaction}
 
 class TransactionController {
 
-  def generateTransaction(account: Account): Unit = {
+  def generateTransaction(currentUser: Account, recipient: String, value: Float): Unit = {
 //    val genesisTransaction = new Transaction(coinbase, account1, 100, coinbase.nonce)
+    val recipientPublicKey = StringUtils.getPublicKeyFromString(recipient)
+    val userPublicKeyStr = StringUtils.getStringFromKey(currentUser.publicKey)
+    val accountState = GlobalAccountState.getAccountState(userPublicKeyStr)
+    val transaction = currentUser.transfer(recipientPublicKey, value, accountState.nonce)
   }
 
 }
